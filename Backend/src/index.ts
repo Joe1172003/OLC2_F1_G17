a29f374f260f5e9a79728fc739269869398f925f
ariellopezg <ajosuelgalvez@gmail.com>
2024-12-05 17:46:58 -0600

    error detection

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,14 @@ import * as Parser from './grammar.js'
 
 const filePath = process.argv[2];
 let data = readFileSync(filePath, { encoding: 'utf-8' });
-const output = Parser.parse(data.trim());
 
-console.log(output);
+try {
+    const output = Parser.parse(data.trim());
+    console.log(output);   
+} catch (error) {
+    if (error.name === "SyntaxError") {
+        console.error(`Error de sintaxis en la línea: ${error.location.start.line}, columna: ${error.location.start.column}. ${error.message}`);
+    } else {
+        console.error("Ocurrió un error inesperado:", error);
+    }
+}
