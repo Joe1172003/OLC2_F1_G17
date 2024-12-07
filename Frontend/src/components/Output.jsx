bb2d68db5d46e5de9413c6202c30a8f2aac92306
ariellopezg <ajosuelgalvez@gmail.com>
2024-12-06 18:11:49 -0600

    update frontend

--- Frontend/src/components/Output.jsx
+++ Frontend/src/components/Output.jsx
@@ -1,6 +1,5 @@
 import {Box, Text, Button, useToast, Icon} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
-import parseInput from "../../analyzer/grammar";
 
 const Output = ({editorRef, outputRef, getOutput}) => {
     const toast = useToast();
@@ -11,10 +10,12 @@ const Output = ({editorRef, outputRef, getOutput}) => {
         setOutput(outputRef);
     }, [outputRef]);
 
-    const execute_parser = () => {
-        const result_parser = parseInput(editorRef.getValue())
-        setOutput(result_parser)
-        getOutput(result_parser)
+    const add_break = (output, number)=>{
+        let result = "";
+        for (let i = 0; i < output.length; i += number) {
+            result += output.slice(i, i + number) + "\n";
+        }
+        return result;
     }
 
     return (
@@ -26,7 +27,6 @@ const Output = ({editorRef, outputRef, getOutput}) => {
                 isLoading={isLoading}
                 borderColor={'rgba(99,179,237,1)'}
                 color={'rgba(99,179,237,1)'}
-                onClick={execute_parser}
             >
                 Analize Code
             </Button>
@@ -40,14 +40,12 @@ const Output = ({editorRef, outputRef, getOutput}) => {
             >
                 <pre>
                     {output && output.length > 0
-                        ? <Text color={output[1] == 'error' ? 'red' : '#73ed3a'} fontWeight={'semibold'} fontStyle={'italic'}>{output[0]}</Text>
+                        ? <Text color={output[1] == 'error' ? 'red' : '#73ed3a'} fontWeight={'semibold'} fontStyle={'italic'}>{add_break(output[0], 60)}</Text>
                         : 'Click "Analize Code" to see the result'}
-                </pre>    
+                </pre> 
             </Box>
         </Box>
     );
 };
-
-
 
 export default Output;
