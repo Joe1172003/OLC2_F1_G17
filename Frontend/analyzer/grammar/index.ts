5b19b2d98dcca49da294e8eb780de14697cc105a
jaguzaro <3013146910101@ingenieria.usac.edu.gt>
2024-12-08 00:51:35 -0600

    test pipeline

--- Frontend/analyzer/grammar/index.ts
+++ Frontend/analyzer/grammar/index.ts
@@ -5,10 +5,10 @@ export default function parseInput(input: string) {
         Parser.parse(input);
     } catch (error) {
         if (error.name === "SyntaxError") {
-            return [`𒉽 Error of sintaxis in line : ${error.location.start.line}, column: ${error.location.start.column}. ${error.message}`, 'error'];
+            return [`𒉽 Error of sintaxis in line: ${error.location.start.line}, column: ${error.location.start.column}. ${error.message}`, 'error'];
         } else {
             return ["𒉽 An unexpected error occurred:" + error, 'error'];
         }
     }
-    return ['✔ Succefull grammar!! :)', 'success'];
+    return ['✔ Succefull grammar!!:)', 'success'];
 }
